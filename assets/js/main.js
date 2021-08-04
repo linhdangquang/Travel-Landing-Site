b768cda47ee77d9521a0cc44cc40d75e8a004e2f
Linhdq <nogamenolife.why@gmail.com>
2021-08-04 14:28:54 +0700

    change

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,5 +49,38 @@ var swiper = new Swiper(".discover__container", {
     },
   });
 
+//   VIDEO
+const videoFile = document.getElementById('video-file'),
+      videoButton = document.getElementById('video-button'),
+      videoIcon = document.getElementById('video-icon')
+
+function playPause(){
+    if (videoFile.paused) {
+        // Play video
+        videoFile.play()
+        
+        videoIcon.classList.add('ri-pause-line')
+        videoIcon.classList.remove('ri-play-line')
+    } else{
+        // Pause
+        videoFile.pause()
+
+        // Change icon again
+        videoIcon.classList.remove('ri-pause-line')
+        videoIcon.classList.add('ri-play-line')
+    }
+}
+
+videoButton.addEventListener('click', playPause)
+
+function finalVideo(){
+    // Video ends, icon change
+    videoIcon.classList.remove('ri-pause-line')
+    videoIcon.classList.add('ri-play-line')
+}
+
+// When video ends
+videoFile.addEventListener('ended', finalVideo)
+
 
 
