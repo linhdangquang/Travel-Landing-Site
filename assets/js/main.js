7b02e085b06b222e4d3dbc28ed2824790b7c2cfe
Linhdq <nogamenolife.why@gmail.com>
2021-08-06 22:04:40 +0700

    add subscribe, footer, sponsor

--- assets/js/main.js
+++ assets/js/main.js
@@ -82,5 +82,33 @@ function finalVideo(){
 // When video ends
 videoFile.addEventListener('ended', finalVideo)
 
+// =================SCROLL UP================//
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When scrollUp is higher 200 pixels
+    if(this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUp)
+
+// ==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
 
 
